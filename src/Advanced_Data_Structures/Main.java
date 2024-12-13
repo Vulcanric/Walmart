eb348b68f7b54000d5753bc79192b1b41adc3bef
Vulcanric <jneric49@gmail.com>
2024-12-13 05:36:53 +0100

    Test heap

---
+++ src/Advanced_Data_Structures/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        PowerOfTwoMaxHeap heap = new PowerOfTwoMaxHeap(3);
+
+        heap.insert(5);
+        heap.insert(2);
+        heap.insert(1);
+        heap.insert(6);
+        heap.printHeap();
+//        System.out.println(heap.popMax());
+//        System.out.println(heap);
+//        heap.insert(9);
+//        heap.insert(0);
+//        System.out.println(heap);
+//        heap.popMax();
+//        System.out.println(heap);
+//        heap.popMax();
+//        System.out.println(heap);
+//        heap.popMax();
+//        System.out.println(heap);
+//        heap.popMax();
+//        System.out.println(heap);
+//        heap.popMax();
+//        System.out.println();
+//        String var = String.format("%s %d", "Number", 5);
+//        heap.popMax();
+//        System.out.println(heap);
+//        heap.popMax();
+    }
+}
